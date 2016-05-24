ee633b7ab6783ad1e05f36e9b421010d50c2e98f
Pablo Acuña <pacupa@users.noreply.github.com>
2016-05-24 12:39:36 -0400

    Mensaje de inicialización

--- SpeedTest.sh
+++ SpeedTest.sh
@@ -7,7 +7,7 @@
 URLs=("http://test.copaco.com.py/click.test" "http://speedtest.reliableservers.com/1GBtest.bin" "http://speedtest.tele2.net/1GB.zip" "http://ipv4.download.thinkbroadband.com/1GB.zip" )
 
 SpeedTest(){
-        echo "Inicializando el SpeedTest Seleccionado..."
+        echo "Inicializando el SpeedTest Seleccionado... " $2
         curl -o /dev/null ${URLs[$1]}
         echo "SpeedTest Finalizado."
 }
@@ -20,19 +20,19 @@ select opt in  "${opciones[@]}"
 do
         case $opt in
         ${opciones[0]} )
-                SpeedTest 0
+                SpeedTest 0 $opt
         ;;
         ${opciones[1]} )
-                SpeedTest 1
-        ;;
+                SpeedTest 1 $opt
+                ;;
         ${opciones[2]} )
-                SpeedTest 2
+                SpeedTest 2 $opt
         ;;
         ${opciones[3]} )
-                SpeedTest 3
+                SpeedTest 3 $opt
         ;;
         ${opciones[4]} )
-                echo Salir... ; break
+                echo Saliendo... ; break
         ;;
         *) echo "Opcion invalida";;
         esac
